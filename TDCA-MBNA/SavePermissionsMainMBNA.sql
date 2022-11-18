48079ea7ba5fba1051a1bf05b70de05809081003
javimanred <70028155+javimanred@users.noreply.github.com>
2022-11-18 11:49:31 -0500

    Create SavePermissionsMainMBNA.sql

---
+++ TDCA-MBNA/SavePermissionsMainMBNA.sql
@@ -0,0 +1,9 @@
+EXEC OGP_Compas_Support.dba.sp_DBPermissions @DBName = 'Compas_TDBFG_MBNA', @Output = 'CreateOnly' ,@Save = 1
+GO
+EXEC OGP_Compas_Support.dba.sp_DBPermissions @DBName = 'Compas_TDBFG_MBNA_Evaluations', @Output = 'CreateOnly' ,@Save = 1
+GO
+EXEC OGP_Compas_Support.dba.sp_DBPermissions @DBName = 'Compas_TDBFG_MBNA_Main', @Output = 'CreateOnly' ,@Save = 1
+GO
+EXEC OGP_Compas_Support.dba.sp_DBPermissions @DBName = 'Compas_TDBFG_MBNA_Reports', @Output = 'CreateOnly' ,@Save = 1
+GO
+EXEC OGP_Compas_Support.dba.sp_DBPermissions @DBName = 'Compas_TDBFG_MBNA_Security', @Output = 'CreateOnly' ,@Save = 1
