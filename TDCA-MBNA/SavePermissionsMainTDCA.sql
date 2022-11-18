ec899e11d6359e22e3f4cd671b8cf061646defbc
javimanred <70028155+javimanred@users.noreply.github.com>
2022-11-18 11:50:41 -0500

    Create SavePermissionsMainTDCA.sql

---
+++ TDCA-MBNA/SavePermissionsMainTDCA.sql
@@ -0,0 +1,9 @@
+EXEC OGP_Compas_Support.dba.sp_DBPermissions @DBName = 'Compas_TDBFG_TDCA', @Output = 'CreateOnly' ,@Save = 1
+GO																	 
+EXEC OGP_Compas_Support.dba.sp_DBPermissions @DBName = 'Compas_TDBFG_TDCA_Evaluations', @Output = 'CreateOnly' ,@Save = 1
+GO																	 
+EXEC OGP_Compas_Support.dba.sp_DBPermissions @DBName = 'Compas_TDBFG_TDCA_Main', @Output = 'CreateOnly' ,@Save = 1
+GO																	 
+EXEC OGP_Compas_Support.dba.sp_DBPermissions @DBName = 'Compas_TDBFG_TDCA_Reports', @Output = 'CreateOnly' ,@Save = 1
+GO																	 
+EXEC OGP_Compas_Support.dba.sp_DBPermissions @DBName = 'Compas_TDBFG_TDCA_Security', @Output = 'CreateOnly' ,@Save = 1
