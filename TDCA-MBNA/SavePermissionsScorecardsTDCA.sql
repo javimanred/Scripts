8050953cacc7df0c44b92068d5ed15b26f7fbd54
javimanred <70028155+javimanred@users.noreply.github.com>
2022-11-18 11:57:21 -0500

    Update SavePermissionsScorecardsTDCA.sql

--- TDCA-MBNA/SavePermissionsScorecardsTDCA.sql
+++ TDCA-MBNA/SavePermissionsScorecardsTDCA.sql
@@ -1,1 +1,1 @@
-EXEC OGP_Compas_Support.dba.sp_DBPermissions @DBName = 'Compas_TDBFG_MBNA_Scorecards', @Output = 'CreateOnly' ,@Save = 1
+EXEC OGP_Compas_Support.dba.sp_DBPermissions @DBName = 'Compas_TDBFG_TDCA_Scorecards', @Output = 'CreateOnly' ,@Save = 1
